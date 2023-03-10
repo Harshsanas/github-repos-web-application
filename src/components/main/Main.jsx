2d5f271056cad8a9853b87154a2387db438fd2eb
harshsanas <harrysanas140@gmail.com>
2023-03-11 00:30:57 +0530

    Initializing react application

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as S from "./main.style"
+
+export default function Main() {
+    return (
+        <S.MainContainer>
+            <div>Main</div>
+        </S.MainContainer>
+    )
+}
